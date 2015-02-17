8cb7a1e388e7b114a268f0b3be98aee779e2b109
Anish Katukam <anishkatukam@sfhs.com>
2015-02-16 20:38:05 -0800

    Undo the manual change, changed Inventory

--- RPG/Inventory.java
+++ RPG/Inventory.java
@@ -2,7 +2,18 @@
  * Add method to open and modify inventory
  * Add method to add an item
  * Add method to check if there's an item in the inventory
+ * 
+ * If an item in a slot is removed, move everything down so that there are no empty spaces before the
+ * ending of items.
    */
 public class Inventory
 {
+    Item[] backpack = new Item[50];
+    
+    public void modifyInventory(){
+    }
+    public void addItem(Item add){
+    }
+    public void checkItem (Item check){
+    }
 }

6220690c414ec8a148766d26be323de1d19ded70
sidmani <Sid@Sids-iMac.local>
2015-02-22 12:08:47 -0800

    Added pick up item method to entity, added stats to armor

--- RPG/Entity.java
+++ RPG/Entity.java
@@ -2,6 +2,8 @@ public class Entity
 {
     private int health;
     private int bloodlust;
+    private int defense;
+    private int mana;
     private Weapon weapon;
     private Hat hat;
     private Potion potion;
@@ -27,5 +29,28 @@ public class Entity
      public Armor getArmor()
     {
         return this.armor;
+    }
+    private void pickUp(Item x)
+    {
+      if (x instanceof Armor)
+      {
+          armor = (Armor) x;
+      }
+      else if (x instanceof Potion)
+      {
+          potion = (Potion) x;
+      }
+        else if (x instanceof Weapon)
+      {
+          weapon = (Weapon) x;
+      }
+        else if (x instanceof Hat)
+      {
+          hat = (Hat) x;
+      }
+        else if (x instanceof Ring)
+      {
+          ring = (Ring) x;
+      }
     }
 }

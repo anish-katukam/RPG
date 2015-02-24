c23cb5f5566944b0700a10d7985cd1961c878758
sidmani <Sid@Sids-iMac.local>
2015-02-23 19:49:08 -0800

    MAJOR ADDITIONS Battle function is pretty much good now. The terms "defense" and "attack" have been generalized across pretty much all the classes to improve fluency. Edited Inventory to use accessor methods. Added a "consume" method for potions in the Entity class. The Potion class's health value should be the amount it restores. Have not implemented hregen. Contact me before making further edits.

--- RPG/Entity.java
+++ RPG/Entity.java
@@ -6,70 +6,81 @@ public class Entity
     private Ring ring;
     private Armor armor;
     private Stats stats;
-    public static void Entity(String name)
+    public Entity(String name)
     {
         stats = new Stats(name);
-        weapon = new weapon_t1;
-        hat = new hat_t1;
-        potion = new potion_t1;
-        ring = new ring_t1;
-        armor = new armor_t1;
+        weapon = new weapon_t1();
+        hat = new hat_t1();
+        potion = new potion_t1();
+        ring = new ring_t1();
+        armor = new armor_t1();
     }
+
     public Weapon getWeapon()
     {
         return this.weapon;
     }
+
     public Hat getHat()
     {
         return this.hat;
     }
-     public Potion getPotion()
+
+    public Potion getPotion()
     {
         return this.potion;
     }
+
     public Ring getRing()
     {
         return this.ring;  
     }
-     public Armor getArmor()
+
+    public Armor getArmor()
     {
         return this.armor;
     }
+
     public Stats getStats()
     {
         return this.stats;
     }
+
     public void pickUp(Item x)
     {
-      if (x instanceof Armor)
-      {
-          armor = (Armor) x;
-      }
-      else if (x instanceof Potion)
-      {
-          potion = (Potion) x;
-      }
+        if (x instanceof Armor)
+        {
+            armor = (Armor) x;
+        }
+        else if (x instanceof Potion)
+        {
+            potion = (Potion) x;
+        }
         else if (x instanceof Weapon)
-      {
-          weapon = (Weapon) x;
-      }
+        {
+            weapon = (Weapon) x;
+        }
         else if (x instanceof Hat)
-      {
-          hat = (Hat) x;
-      }
+        {
+            hat = (Hat) x;
+        }
         else if (x instanceof Ring)
-      {
-          ring = (Ring) x;
-      }
+        {
+            ring = (Ring) x;
+        }
     }
+
+    public void consume(Potion a)
+    {
+        this.stats.setHealth(this.stats.getHealth() + a.getHealth());
+        this.stats.setEnergy(this.stats.getEnergy() + a.getEnergy());
+        this.stats.setBloodlust(this.stats.getBloodlust() + a.getBloodlust());
+    }
+
     public int attack (Entity a)
     {
-       int damage = 100 * (this.getWeapon().getDamage() / a.getArmor().getResistance());
-       a.getStats().takeHealthDamage(damage);
-       return damage;
-    }
-    public void updateStats()
-    {
-        
+        int damage = 100 * (this.getStats().getAttack() / a.getStats().getDefense);
+        a.getStats().takeHealthDamage(damage);
+        return damage;
     }
 }

3b4380bd31cd0de99a28a0b91a92cf4b81be2a55
Anish Katukam <anish.katukam@yahoo.com>
2015-03-01 18:08:32 -0800

    V2 - Added Resources folder for sprites, as well as a Graphics class

---
+++ RPG/Graphics.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Graphics extends JComponent
+{
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        
+        
+    }
+}
--- RPG/Starter.java
+++ RPG/Starter.java
@@ -1,6 +1,11 @@
 public class Starter
 {
     public static void main (String Args[]){
+        JFrame frame = new JFrame();
+        frame.setSize(1920,1080);
+        frame.setTitle("Swords and Sorcery");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         GameLogic g = new GameLogic(); //instantiates new gamelogic
         g.exposition(); 
         g.intro();

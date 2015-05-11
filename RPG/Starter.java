3e11c0667e1046cf4719d09f53a452f5613954cd
sukritarora <sukrit.arora2211@gmail.com>
2015-05-11 14:58:52 -0700

    Added full screen icon

--- RPG/Starter.java
+++ RPG/Starter.java
@@ -5,29 +5,37 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.*;
+//import javafx.scene.media.Media;
+//import javafx.scene.media.MediaPlayer;
+//import javafx.embed.swing.JFXPanel;
+//import javafx.scene.*;
 
 public class Starter
 {
     public static void main (String Args[]){
         JFrame frame = new JFrame("Swords & Sworcery");               
-        frame.setSize(1920,1080);
+        frame.setSize(1500,900);
         frame.setUndecorated(true);
 
 
-        final JFXPanel fxPanel = new JFXPanel(); 
-        frame.add(fxPanel); 
-        initFX(fxPanel); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// //        final JFXPanel fxPanel = new JFXPanel(); 
+//         frame.add(fxPanel); 
+//         initFX(fxPanel); 
+//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.add(new OpeningSplash());
+        //frame.add(new OpeningSplash());
+        JButton close = new JButton("Close");
+        close.setOpaque(false);
+        close.setContentAreaFilled(false);
+        close.setBorderPainted(false);
+        JPanel masterPanel = new JPanel(new BorderLayout());
+        masterPanel.add(new OpeningSplash(), BorderLayout.CENTER);
+        masterPanel.add(close, BorderLayout.NORTH);
+        frame.add(masterPanel);
         frame.setVisible(true);
     }
 
-    private static void initFX(JFXPanel fxPanel) {
-    }
+ //   private static void initFX(JFXPanel fxPanel) {
+ //   }
 
 }

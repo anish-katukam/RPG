1effc6cf6e68ac54b31320808a4793800c6d3e8a
sukritarora <sukrit.arora2211@gmail.com>
2015-05-12 22:16:09 -0700

    Goodnight folks... A pretty productive day

--- RPG/Starter.java
+++ RPG/Starter.java
@@ -14,11 +14,28 @@ public class Starter
 {
     static final JFrame frame = new JFrame("Swords & Sworcery");
     public static void main (String Args[]){
+
                         
-        frame.setSize(1920,1080);
-        JLayeredPane buttonOptionPane = new JLayeredPane();
-        
-        
+       
+        frame.setSize(1920, 1080);
+        final JLayeredPane layeredPane = new JLayeredPane();
+        final JPanel menu = new JPanel(new GridLayout(6, 1));
+        menu.setBounds(500, 150, 500, 500);
+        JButton option1 = new JButton("option1");
+        JButton option2 = new JButton("option2");
+        JButton option3 = new JButton("option3");
+        JButton option4 = new JButton("option4");
+        JButton option5 = new JButton("option5");
+        JButton option6 = new JButton("option6");
+        menu.add(option1);
+        menu.add(option2);
+        menu.add(option3);
+        menu.add(option4);
+        menu.add(option5);
+        menu.add(option6);
+        final JButton menuButton = new JButton("Menu");
+        menuButton.setBounds(1350, 60, 50, 50);
+
 
         // //        final JFXPanel fxPanel = new JFXPanel(); 
         //         frame.add(fxPanel); 
@@ -28,50 +45,82 @@ public class Starter
         //frame.add(new OpeningSplash());
         //JButton fullScreenButton = new JButton(new ImageIcon("Full_Screen_Icon.png"));
 
-        JButton fullScreenButton = new JButton(new ImageIcon("Full_Screen_Icon.png"));
+        final JButton fullScreenButton = new JButton(new ImageIcon("Full_Screen_Icon.png"));
         fullScreenButton.setOpaque(false);
         fullScreenButton.setContentAreaFilled(false);
         fullScreenButton.setBorderPainted(false);
         fullScreenButton.setBounds(1350, 0, 50, 50);
-        
-        
-        
+
         //JPanel masterPanel = new JPanel(new BorderLayout());
         //frame.add(new OpeningSplash());
-        //buttonOptionPane.add(new OpeningSplash(), JLayeredPane.DEFAULT_LAYER);
+        //layeredPane.add(new OpeningSplash(), JLayeredPane.DEFAULT_LAYER);
         //frame.add(new OpeningSplash());
         OpeningSplash image = new OpeningSplash();
+
         image.setBounds(0, 0, 1920, 1080);
-        buttonOptionPane.add(image, new Integer(0));
-        buttonOptionPane.add(fullScreenButton, new Integer(1));
+        
         
         //buttonOptionPane.add(test, new Integer(0));
+       
+        layeredPane.add(menuButton, new Integer(1));
+
+        //layeredPane.add(test, new Integer(0));
+
         //masterPanel.add(fullScreenButton, BorderLayout.NORTH);
         //frame.add(masterPanel);
         //frame.add(new OpeningSplash());
-        frame.add(buttonOptionPane);
+        frame.add(layeredPane);
         frame.setUndecorated(true);
         frame.setVisible(true);
-        
+
         //The buttonlistnere class will be used later to do stuff with the minimize and fullScreenButton buttons
         class ButtonListener implements ActionListener
         {
             public void actionPerformed(ActionEvent e)
             {
-                frame.dispose();
-                frame.setUndecorated(!frame.isUndecorated());
-                frame.pack();
-                frame.setSize(1500,900);
-                frame.setVisible(true);
-                
+                if (e.getSource() == fullScreenButton)
+                {
+                    frame.dispose();
+                    frame.setUndecorated(!frame.isUndecorated());
+                    frame.pack();
+                    frame.setSize(1500,900);
+                    frame.setVisible(true);
+                }
+
+                if (e.getSource() == menuButton)
+                {
+
+                    layeredPane.add(menu, new Integer(2));
+
+                     
+                }
+
             }
         }
-        
+
+        class MenuListener implements KeyListener
+        {
+            public void keyPressed(KeyEvent e)
+            { 
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+                {
+                    System.out.println("HI");
+                    layeredPane.add(menu, new Integer(2));
+
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {}
+
+            public void keyTyped(KeyEvent e) {}
+        }
+
         fullScreenButton.addActionListener(new ButtonListener());
-        
-        
+        menuButton.addActionListener(new ButtonListener());
+        image.setFocusable(true);
+        image.addKeyListener(new MenuListener());
+
     }
-   
 
     //   private static void initFX(JFXPanel fxPanel) {
     //   }

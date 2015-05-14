c9c726d40eb35694a687ddb4da8f97a32dd7534c
sidmani <Sid@Sids-iMac.local>
2015-05-14 16:31:57 -0700

    Removed unnecessary

--- RPG/Backgrounds.java
+++
@@ -1,9 +0,0 @@
-//This class should replace the splash art classes. It should be able to display splashes, as well as
-//backgrounds (gifs) for the levels.
-
-public class Backgrounds
-{
-    public Backgrounds()
-    {
-    }
-}

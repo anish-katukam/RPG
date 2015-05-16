b5477a3b27c081830bb9ac06dba4600c3ec482cc
sidmani <Sid@Sids-iMac.local>
2015-05-16 09:39:56 -0700

    Added basic piecewise movement functionality using the ternary operator.

--- RPG/Starter.java
+++ RPG/Starter.java
@@ -15,6 +15,7 @@ public class Starter
     static final JFrame frame = new JFrame("Swords & Sworcery");
     static final OurGraphics graphics = new OurGraphics();
     static final JFileChooser fileChooser = new JFileChooser();
+    static private int currentFrame = 0;
     public static void main (String Args[]){
 
         frame.setSize((int)util.screen_size.getWidth(),(int)util.screen_size.getHeight());
@@ -23,9 +24,9 @@ public class Starter
 
         String response = (String) JOptionPane.showInputDialog(null, "Would you like to Load a Saved Game?", "Load Game?", JOptionPane.PLAIN_MESSAGE,null, yesOrNo, yesOrNo[0]);
         if (response.equals("Yes")) {
-            
+
             System.out.println("This is working");
-            
+
             int userSelection = fileChooser.showOpenDialog(new JLabel());
 
             if (userSelection == JFileChooser.APPROVE_OPTION) {
@@ -43,29 +44,34 @@ public class Starter
             }
         }
         else    {
-            
-        }
-
-
-            // //        final JFXPanel fxPanel = new JFXPanel(); 
-            //         frame.add(fxPanel); 
-            //         initFX(fxPanel); 
-            //         frame.setDefaultfullScreenButtonOperation(JFrame.EXIT_ON_fullScreenButton);
-
-            GameLogic.character.setSprites(new String[] {"Resources/sprite_test.png"});
-            GameLogic.character.setPosition(100,100);
-            GameLogic.character.set_curr_sprite(0);
-
-            //e.moveTo(new Point(500,500), 5000);
-            graphics.addEntity(GameLogic.character);
-            frame.add(graphics.getGraphics(0));
-            frame.setUndecorated(true);
-
-            frame.setVisible(true);
-
 
         }
-        //   private static void initFX(JFXPanel fxPanel) {
-        //   }
+
+        // //        final JFXPanel fxPanel = new JFXPanel(); 
+        //         frame.add(fxPanel); 
+        //         initFX(fxPanel); 
+        //         frame.setDefaultfullScreenButtonOperation(JFrame.EXIT_ON_fullScreenButton);
+
+        GameLogic.character.setSprites(new String[] {"Resources/sprite_test.png"});
+        GameLogic.character.setPosition(100,100);
+        GameLogic.character.set_curr_sprite(0);
+
+        //e.moveTo(new Point(500,500), 5000);
+        graphics.addEntity(GameLogic.character);
+        frame.add(graphics.getGraphics(0));
+        frame.setUndecorated(true);
+
+        frame.setVisible(true);
 
     }
+    //   private static void initFX(JFXPanel fxPanel) {
+    //   }
+    public static int getCurrentFrame()
+    {
+        return currentFrame;
+    }
+    public static void setCurrentFrame(int f)
+    {
+        currentFrame = f;
+    }
+}

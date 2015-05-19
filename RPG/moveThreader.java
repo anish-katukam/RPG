8925ca084cf2f3c68cb248b61e5ddbbfe837fb83
sidmani <Sid@Sids-iMac.local>
2015-05-19 13:11:12 -0700

    commented all my classes

--- RPG/moveThreader.java
+++ RPG/moveThreader.java
@@ -5,33 +5,36 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
-public class moveThreader extends Thread
+//////
+////class to animate movement of the character////
+//////
+public class moveThreader extends Thread //creates new thread when constructed
 {
-    private volatile int d;
-    private volatile Point p;
-    private volatile Entity e;
-    private static volatile boolean cancel = false;
+    private volatile int d; //duration of movement
+    private volatile Point p; //point to move to
+    private volatile Entity e; //entity to move
+    private static volatile boolean cancel = false; //cancel all threads
     public moveThreader(Entity _e, Point _p, int _d)
     {
-        e=_e;
+        e=_e; // set explicit variables to those passed to the constructor
         p=_p;
         d=_d;
-        this.start();
+        this.start(); //start the thread
     }
 
-    public void run()
+    public void run() //method called by the threader
     {
-        cancel = false;
-        long millis = System.currentTimeMillis();
-        double x_orig = e.getPosition().getX();
+        cancel = false; 
+        long millis = System.currentTimeMillis(); //get start time
+        double x_orig = e.getPosition().getX(); // get original x and y coords
         double y_orig = e.getPosition().getY();
-        double x_distance = p.getX() - e.getPosition().getX();
+        double x_distance = p.getX() - e.getPosition().getX(); //get total distance to move on x and y axis
         double y_distance = p.getY() - e.getPosition().getY();
-        double curr_time = System.currentTimeMillis() - millis;
-        while (curr_time < d && !cancel)
+        double curr_time = System.currentTimeMillis() - millis; // get current time, adjusted for start offset
+        while (curr_time < d && !cancel) //while not canceled and not out of time
         {
-            curr_time = System.currentTimeMillis() - millis;
-            e.setPosition((int)(((curr_time/d))*x_distance+x_orig), (int)(((curr_time/d))*y_distance+y_orig));
+            curr_time = System.currentTimeMillis() - millis; //uodate time
+            e.setPosition((int)(((curr_time/d))*x_distance+x_orig), (int)(((curr_time/d))*y_distance+y_orig)); // determine current distance  as a ratio of elapsed time/total time
            //e.setPosition((int)(((curr_time/d))*x_distance+x_orig), piecewise_handler.getY((int)(((curr_time/d))*x_distance+x_orig), Starter.getCurrentFrame()));
         }
 
@@ -42,6 +45,6 @@ public class moveThreader extends Thread
     public static void cancel()
     {
         
-        cancel = true;
+        cancel = true; //cancel all threads
     }
 }
--- RPG/util.java
+++ RPG/util.java
@@ -7,27 +7,27 @@ import javax.imageio.*;
 import javax.swing.*;
 public abstract class util
 {
-    public static final Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
-    static Point map_to_frame(JFrame frame, Point p)
+    public static final Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize(); //system screen size
+    static Point map_to_frame(JFrame frame, Point p) //map a point on the image in 1920,1080 space to the screen
     {
-        Dimension d = frame.getContentPane().getSize();
-        int x = (int)((d.getWidth()*(p.getX()))/(screen_size.getWidth()));
+        Dimension d = frame.getContentPane().getSize(); //get frame size
+        int x = (int)((d.getWidth()*(p.getX()))/(screen_size.getWidth())); //use ratio to map point x and y
         int y = (int)((d.getHeight()*(p.getY()))/(screen_size.getHeight()));
-        p.setLocation(x,y);
-        return p;
+        p.setLocation(x,y); //set location of the point
+        return p; // return the point
 
     }
 
-    static BufferedImage resize_img(Dimension d, BufferedImage i)
+    static BufferedImage resize_img(Dimension d, BufferedImage i) //resize an image to a dimension d
     {
-
+        //uses the getScaledInstance method of buffered image to provide a scaled version of the image
         return toBufferedImage(i.getScaledInstance((int)d.getWidth(),(int)d.getHeight(), Image.SCALE_FAST));
 
     }
-    public static Point screen_point_adjust(Point p)
+    public static Point screen_point_adjust(Point p) //adjust for screen offset, just calibration
     {
-        p.setLocation(p.getX() - 160, p.getY() -200);
-        return p;
+        p.setLocation(p.getX() - 160, p.getY() -200); //calibrates point
+        return p; //returns output
     }
     public static BufferedImage toBufferedImage(Image img)
     {
@@ -47,13 +47,14 @@ public abstract class util
         // Return the buffered image
         return bimage;
     }
-    public static BufferedImage blur(BufferedImage img)
+    public static BufferedImage blur(BufferedImage img) //gaussian blur method
     {
+        //create an array used to adjust individual pixels for a blur effect
         float[] bits = {1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f};;
-        Kernel kernel = new Kernel(3,3,bits);
-        ConvolveOp convolve_op = new ConvolveOp(kernel);
-        BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
-        convolve_op.filter(img, out);
-        return out;
+        Kernel kernel = new Kernel(3,3,bits); //create a kernel using the offset array created above
+        ConvolveOp convolve_op = new ConvolveOp(kernel); //convolute the pixels using the kernel array
+        BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(), img.getType()); //create new image
+        convolve_op.filter(img, out); //filter img into out using the convolveop created above
+        return out; //return modified image
     }
 }
